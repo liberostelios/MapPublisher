7e310447edf14c67cba5cea7717705344d0fd182
Stelios Vitalis <liberostelios@gmail.com>
2015-06-13 18:14:22 +0300

    Add support for dynamic GeoJSON of ESRI Shapefiles

--- laravel/app/Layer.php
+++ laravel/app/Layer.php
@@ -10,19 +10,64 @@ class Layer extends Model {
 
 	protected $hidden = ['connection_string', 'username', 'password'];
 
-	function wkb_to_json($wkt) {
+	function wkt_to_json($wkt) {
 			$geom = geoPHP::load($wkt,'wkt');
 			return $geom->out('json');
 	}
 
-	public function getGeoJsonAttribute()
-	{
+	public function getGeoJsonAttribute() {
 		$connection_string = $this->attributes['connection_string'];
 		$username = $this->attributes['username'];
 		$password = $this->attributes['password'];
 		$table = $this->attributes['table_name'];
 		$geocolumn = $this->attributes['geometry_field_name'];;
 
+		$conn_type = explode(':', $connection_string)[0];
+		if ($conn_type == "shp") {
+			$geojson = $this->loadShpData($connection_string);
+		} else {
+			$geojson = $this->loadPDOData($connection_string, $username, $password, $table, $geocolumn);
+		}
+
+		header('Content-type: application/json');
+		echo json_encode($geojson, JSON_NUMERIC_CHECK);
+		$conn = NULL;
+	}
+
+	public function loadShpData($connection_string) {
+		$filename = explode(':', $connection_string)[1];
+
+		//$shp = new \App\Libraries\ShpLibrary\ShapeFile($filename, array('noparts' => false));
+		//echo $shp->getError();
+		$shp = new \App\Libraries\ShpParser\ShpParser();
+		$shp->load($filename);
+
+		# Build GeoJSON feature collection array
+		$geojson = array(
+			'type'      => 'FeatureCollection',
+			'features'  => array(),
+			'crs' => array('type' => 'name', 'properties' => array('name' => 'urn:ogc:def:crs:EPSG::2100'))
+		);
+
+		foreach ($shp->getShapeData() as $record) {
+			//dd($record);
+			$properties = array();
+			$feature = array(
+					'type' => 'Feature',
+					'geometry' => json_decode($this->wkt_to_json($record['geom']['wkt'])),
+					'properties' => $properties
+			);
+
+			# Add feature arrays to feature collection array
+			array_push($geojson['features'], $feature);
+		}
+
+		return $geojson;
+	}
+
+	// This function loads the data in case it's a PDO connection
+	public function loadPDOData($connection_string, $username, $password, $table, $geocolumn)
+	{
 		# Connect to MySQL database
 		$conn = new PDO($connection_string, $username, $password);
 
@@ -59,7 +104,7 @@ class Layer extends Model {
 		    unset($properties[$geocolumn]);
 		    $feature = array(
 		         'type' => 'Feature',
-		         'geometry' => json_decode($this->wkb_to_json($row['wkt'])),
+		         'geometry' => json_decode($this->wkt_to_json($row['wkt'])),
 		         'properties' => $properties
 		    );
 
@@ -67,8 +112,6 @@ class Layer extends Model {
 		    array_push($geojson['features'], $feature);
 		}
 
-		header('Content-type: application/json');
-		echo json_encode($geojson, JSON_NUMERIC_CHECK);
-		$conn = NULL;
+		return $geojson;
 	}
 }
